1fdbbda07f836911d4f16ad783819b18603be121
dericko681 <abonghoderick@gmail.com>
2024-09-30 15:39:24 +0100

    a simple game

--- game.sh
+++ game.sh
@@ -1,25 +1,26 @@
-function guessing_game {
-   rand_num=$((RANDOM%$1))
+#!/bin/bash
+# A simple number guessing game script
+guessing_number() {
+   rand_number=$((RANDOM % $1))
+   read -p "Guess a number between 0 and $1: " guest_number
+
    for i in {1..3}; do
-   # while [i -lt 3]; do
-   # i=i+ 1
-      
-      if [ $2 -eq $rand_num]; then 
-         echo "you got it correct, bravo!"
-         return
-      elif [ $rand_num -gt $2]; then
-         echo "you missed!, the number is greater than $2"
-         # read -p "please try again: " | read guess_number
-         # $2 = $guess_number
-
-      else 
-         echo "you missed!, the number is less than $2"
-         # read -p "please try again: " | read guess_number
-         # $2 = $guess_number
+      if [ $rand_number -eq $guest_number ]; then
+         echo "You won!"
+         break
+      elif [ $rand_number -gt $guest_number ]; then
+         echo "You missed! The random number is greater than $guest_number."
+         read -p "Please try again: " guest_number
+      else
+         echo "You missed! The random number is less than $guest_number."
+         read -p "Please try again: " guest_number
       fi
-   done 
-      # if [i -eq 3]; then
-      echo "you lose, the number is $rand_num" 
-      # fi
+   done
 
+   if [ $i -eq 3 ]; then
+      echo "You lose! The correct number was $rand_number."
+   fi
 }
+
+# Call the function with a range (e.g., 10)
+#guessing_number
